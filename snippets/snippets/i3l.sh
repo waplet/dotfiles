9724a4ce64f87a50f2fb16f34435b7ec3826696f
waplet <waplet@gmail.com>
2017-01-30 12:07:57 +0200

    i3 update/ i3lock update, new file

--- snippets/snippets/i3l.sh
+++ snippets/snippets/i3l.sh
@@ -16,7 +16,7 @@ i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e \
         --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 \
         --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c \
         -c "101218"
-# -n
+        -n
 
 # resume message display
 # pkill -u "$USER" -USR2 dunst
